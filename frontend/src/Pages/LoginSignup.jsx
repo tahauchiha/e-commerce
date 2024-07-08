6c65678a460116bfc07e8ce4cd3e89c354e17317
Taha Ashraf Ansari <129264109+tahauchiha@users.noreply.github.com>
2024-07-08 13:05:10 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -17,7 +17,7 @@ const LoginSignup = () => {
   const login = async () =>{
     console.log("Login Function Executed",formData);
     let responseData;
-    await fetch('https://e-commerce-backend-cqbc.onrender.com/login',{
+    await fetch('https://e-commerce-backend-x0j7.onrender.com/login',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
@@ -38,7 +38,7 @@ const LoginSignup = () => {
   const signup = async () =>{
     console.log("Signup Function Executed",formData);
     let responseData;
-    await fetch('https://e-commerce-backend-cqbc.onrender.com/signup',{
+    await fetch('https://e-commerce-backend-x0j7.onrender.com/signup',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
